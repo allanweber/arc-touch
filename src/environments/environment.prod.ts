4c0e221b3776f1c498ac20c549ae52dfdf0a29b1
cwi.allan <cwi.allan@agibank.com.br>
2019-02-02 10:15:55 -0200

    Inital project structure generated by 'ionic start' command

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  movies: 'https://api.themoviedb.org/3',
+  token: '1f54bd990f1cdfb230adb312546d765d',
+  smallImage: 'https://image.tmdb.org/t/p/w45',
+  image: 'https://image.tmdb.org/t/p/w300'
+};

c594b17fdf83b17d69da9b6f85e9be6afd12281a
cwi.allan <cwi.allan@agibank.com.br>
2019-02-03 15:26:00 -0200

    Remove unused things in environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: false,
   movies: 'https://api.themoviedb.org/3',
-  token: '1f54bd990f1cdfb230adb312546d765d',
-  smallImage: 'https://image.tmdb.org/t/p/w92',
-  image: 'https://image.tmdb.org/t/p/w300'
+  token: '1f54bd990f1cdfb230adb312546d765d'
 };

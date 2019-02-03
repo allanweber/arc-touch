9fb355ef967e73cadbd029b9cd29cfa595f4501b
cwi.allan <cwi.allan@agibank.com.br>
2019-02-03 14:31:24 -0200

    Searching components and events

--- src/app/movies/movies-list/movies-list.component.ts
+++ src/app/movies/movies-list/movies-list.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { IonInfiniteScroll } from '@ionic/angular';
+import { IonInfiniteScroll, IonSearchbar, LoadingController } from '@ionic/angular';
 import { Movie } from '../model/movie.model';
 import { MovieService } from '../services/movie.service';
 
@@ -9,12 +9,18 @@ import { MovieService } from '../services/movie.service';
   templateUrl: './movies-list.component.html',
   styleUrls: ['./movies-list.component.scss']
 })
-export class MoviesListcomponent implements OnInit, OnDestroy {
+export class MoviesListcomponent implements OnInit {
   @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
+  @ViewChild(IonSearchbar) searchBar: IonSearchbar;
   public movies: Movie[] = [];
   private currentPage = 1;
+  public isSearching = false;
 
-  constructor(private movieService: MovieService, private router: Router) {}
+  constructor(
+    private movieService: MovieService,
+    private router: Router,
+    public loadingController: LoadingController
+  ) {}
 
   ngOnInit() {
     this.loadMovies(null);
@@ -29,20 +35,41 @@ export class MoviesListcomponent implements OnInit, OnDestroy {
     });
   }
 
-  doRefresh(event) {
-    if (event.cancelable) {
-      event.preventDefault();
-    }
+  loadFromBegining(event) {
+    this.isSearching = false;
     this.currentPage = 1;
     this.movieService.clearMovies();
     this.loadMovies(event);
   }
 
-  loadData(event) {
+  loadMoreData(event) {
     if (event.cancelable) {
       event.preventDefault();
     }
     this.loadMovies(event);
+  }
+
+  refreshList(event) {
+    if (event.cancelable) {
+      event.preventDefault();
+    }
+    this.loadFromBegining(event);
+    this.searchBar.value = null;
+  }
+
+  searchClear(event) {
+    this.loadFromBegining(null);
+  }
+
+  searchChanged(event) {
+    if (this.isSearchEvent(event)) {
+      this.isSearching = true;
+      console.log('searchChanged');
+    }
+  }
+
+  isSearchEvent(event) {
+    return event && event.detail && event.detail.value && event.detail.value != '';
   }
 
   toggleInfiniteScroll() {
@@ -51,9 +78,5 @@ export class MoviesListcomponent implements OnInit, OnDestroy {
 
   openMovie(movie: Movie) {
     this.router.navigate(['/movies', movie.id]);
-  }
-
-  ngOnDestroy() {
-    console.log('ngOnDestroy');
   }
 }
